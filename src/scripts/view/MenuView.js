6503d493bcb1230d9312fdf2274ad86247cd542b
Schildi <philippe.schild@gmail.com>
2013-09-06 00:04:26 +0200

    Menu eingebaut

--- src/scripts/view/MenuView.js
+++ src/scripts/view/MenuView.js
@@ -9,10 +9,42 @@ define(
 		var MenuView = View.extend({
 			$element: $('#menu-container'),
 
+			doBind: function() {
+				$('.menu-toggle').fastClick(this.getToggleMenuClickHandler_());
+				$('.menu-mask').fastClick(this.getMenuMaskClickHandler_());
+				$('#menu a').fastClick(this.getMenuItemClickHandler_());
+			},
+
+			doUnbind: function() {
+				$('.menu-toggle').unbind();
+				$('#menu a').unbind();
+			},
+
 			doRender: function() {
 				this.$element.html(
 					_.template(menuTemplate)
 				);
+			},
+
+			getToggleMenuClickHandler_: function() {
+				var that = this;
+				return function() {
+					that.trigger('menutoggle');
+				}
+			},
+
+			getMenuMaskClickHandler_: function() {
+				var that = this;
+				return function() {
+					that.trigger('menumaskclick');
+				}
+			},
+
+			getMenuItemClickHandler_: function() {
+				var that = this;
+				return function() {
+					that.trigger('menuitemclick');
+				}
 			}
 		});
 		
--- src/scripts/viewModel/MenuViewModel.js
+++ src/scripts/viewModel/MenuViewModel.js
@@ -10,6 +10,29 @@ define(
 
 			doInitialize: function(options) {
 				this.mainView = new MenuView();
+
+				this.listenTo(this.mainView, 'menutoggle', this.handleToggleMenuClick_);
+				this.listenTo(this.mainView, 'menumaskclick', this.handleMenuMaskClick_);
+				this.listenTo(this.mainView, 'menuitemclick', this.handleMenuItemClick_);
+			},
+
+			handleToggleMenuClick_: function() {
+				this.toggleMenu();
+			},
+
+			handleMenuMaskClick_: function() {
+				if ($('.menu-mask').hasClass('visible')) {
+					this.toggleMenu();
+				}
+			},
+
+			handleMenuItemClick_: function() {
+				this.toggleMenu();
+			},
+
+			toggleMenu: function() {
+				$('#menu').toggleClass('menu-open');
+				$('#menu-mask').toggleClass('visible');
 			}
 
 		});
