7343d551d3a82c92f14e6a762cc7940288762736
Schildi <philippe.schild@gmail.com>
2013-09-07 21:53:37 +0200

    Statistik-View rudimentaer eingebaut

--- src/scripts/view/StatisticView.js
+++ src/scripts/view/StatisticView.js
@@ -1,18 +1,93 @@
 define(
 	[
 		'jquery', 'underscore', 'view/View',
-		'text!templates/statistic.html'
+		'mixin/DatetimeMixin',
+		'text!templates/statistic/statistic.html'
 	],
-	function($, _, View, statisticTemplate) {
+	function($, _, View, datetimeMixin, statisticTemplate) {
 		'use strict';
 
 		var StatisticView = View.extend({
 			$element: $('#app-container'),
 
+			doBind: function() {
+				$('#load-statistics-button').fastClick(this.getLoadStatisticsButtonClickHandler_());
+			},
+
+			doUnbind: function() {
+				$('#load-statistics-button').unbind();
+			},
+
+			doPopulate: function(data) {
+				this.fuelsortCollection_ = data.fuelsortCollection;
+				this.populateFuelsorts_();
+			},
+
+			populateFuelsorts_: function() {
+				$('#fuelsort-chooser').append(
+					$('<option/>').val(-1).text('bitte wählen...')
+				);
+
+				_.each(this.fuelsortCollection_.getData(), function(fuelsort) {
+					var option = $('<option/>').val(fuelsort.id).text(fuelsort.name);
+					$('#fuelsort-chooser').append(option);
+				});
+			},
+
 			doRender: function() {
 				this.$element.html(
 					_.template(statisticTemplate)
 				);
+
+				this.initDatetimePicker_();
+				this.setCurrentDateAndTime_();
+			},
+
+			setCurrentDateAndTime_: function() {
+				var fromDate = new Date(new Date() - 1000 * 60 * 60 * 24 * 14);
+				$('#from-date').val(datetimeMixin.getDateAsString(fromDate));
+				$('#to-date').val(datetimeMixin.getCurrentDate());
+			},
+
+			initDatetimePicker_: function() {
+				/* makes the datetime-field loose the focus whenever it is clicked. That's a fix for mobiscroll... */
+				$('#from-date').focus(function() {
+					$('#from-date').blur();
+				});
+
+				$('#from-date').mobiscroll().date({
+					showNow: true,
+					maxDate: new Date(),
+					startYear: new Date().getFullYear() - 2,
+					theme: 'android-ics light',
+					lang: 'de',
+					display: 'modal',
+					animate: 'pop',
+					mode: 'scroller'
+				});
+
+				/* makes the datetime-field loose the focus whenever it is clicked. That's a fix for mobiscroll... */
+				$('#to-date').focus(function() {
+					$('#to-date').blur();
+				});
+
+				$('#to-date').mobiscroll().date({
+					showNow: true,
+					maxDate: new Date(),
+					startYear: new Date().getFullYear() - 2,
+					theme: 'android-ics light',
+					lang: 'de',
+					display: 'modal',
+					animate: 'pop',
+					mode: 'scroller'
+				});
+			},
+
+			getLoadStatisticsButtonClickHandler_: function() {
+				var that = this;
+				return function() {
+					that.trigger('loadstatistics');
+				}
 			}
 		});
 		
--- src/scripts/viewModel/StatisticViewModel.js
+++ src/scripts/viewModel/StatisticViewModel.js
@@ -1,15 +1,57 @@
 define(
 	[
 		'underscore',
-		'viewModel/ViewModel', 'view/StatisticView'
+		'viewModel/ViewModel',
+		'Config', 'mixin/DatetimeMixin',
+		'collection/FuelsortCollection',
+		'view/StatisticView'
 	],
-	function(_, ViewModel, StatisticView) {
+	function(_, ViewModel, config, datetimeMixin, FuelsortCollection, StatisticView) {
 		'use strict';
 
 		var StatisticViewModel = ViewModel.extend({
 
 			doInitialize: function(options) {
 				this.mainView = new StatisticView();
+
+				this.listenTo(this.mainView, 'loadstatistics', this.handleLoadStatisticsButtonClick_);
+			},
+
+			doPopulate: function() {
+				this.mainView.populate({
+					fuelsortCollection: FuelsortCollection
+				});
+			},
+
+			handleLoadStatisticsButtonClick_: function() {
+				var fuelsortId = $('#fuelsort-chooser').val();
+				var gasstationId = $('#location-chooser').val() || -1; // TODO
+
+				var fromDate = datetimeMixin.getDateTimeForServer(
+					datetimeMixin.formatGermanDatetimeToJsDate($('#from-date').val(), false),
+					false
+				);
+
+				var toDate = datetimeMixin.getDateTimeForServer(
+					datetimeMixin.formatGermanDatetimeToJsDate($('#to-date').val(), false),
+					false
+				);
+
+				$.ajax({
+					url: config.baseUrl + 'statistics.php/statistic',
+					data: {
+						'fuelsortId': parseInt(fuelsortId),
+						'gasstationId': parseInt(gasstationId),
+						'fromDate': fromDate,
+						'toDate': toDate
+					},
+					success: function(r) {
+						console.log('success', r);
+					},
+					error: function() {
+						alert('Ups...beim Laden der Statistiken ist wohl etwas schief gelaufen :-(');
+					}
+				});
 			}
 
 		});
