f10dac842b1a4378b30e8b1ce97b279081badcba
Schildi <philippe.schild@googlemail.com>
2013-11-06 17:18:04 +0100

    MTS-Service auf neuem Server implementiert. Da nun Skript und DB auf dem gleichen Server liegen, ist der MTS-Receiver nicht mehr notwendig

--- src/data/php/mts.php
+++ src/data/php/mts.php
@@ -1,4 +1,6 @@
 <?php
+require 'connection.php';
+
 /**
  * http://www.spritpreismonitor.de/suche
  * PLZ = 47533
@@ -8,42 +10,117 @@ $superUrl = 'http://www.spritpreismonitor.de/suche/?tx_spritpreismonitor_pi1%5Bs
 $dieselUrl = 'http://www.spritpreismonitor.de/suche/?tx_spritpreismonitor_pi1%5BsearchRequest%5D%5BplzOrtGeo%5D=47533&tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bumkreis%5D=25&tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bkraftstoffart%5D=diesel&tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Btankstellenbetreiber%5D=';
 $e10Url = 'http://www.spritpreismonitor.de/suche/?tx_spritpreismonitor_pi1%5BsearchRequest%5D%5BplzOrtGeo%5D=47533&tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bumkreis%5D=25&tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Bkraftstoffart%5D=e10&tx_spritpreismonitor_pi1%5BsearchRequest%5D%5Btankstellenbetreiber%5D=';
 
-if ((int) $_GET['fuelsortId'] === 1) {
-	applyPrices($superUrl, 1, 'e5');
-} else if ((int) $_GET['fuelsortId'] === 3) {
-	applyPrices($dieselUrl, 3, 'diesel');
-} else if ((int) $_GET['fuelsortId'] === 5) {
-	applyPrices($e10Url, 5, 'e10');
+if ((int)$_GET['fuelsortId'] === 1) {
+	$data = extractData($superUrl, 'e5');
+	applyData(1, $data);
+} else if ((int)$_GET['fuelsortId'] === 3) {
+	$data = extractData($dieselUrl, 'diesel');
+	applyData(3, $data);
+} else if ((int)$_GET['fuelsortId'] === 5) {
+	$data = extractData($e10Url, 'e10');
+	applyData(5, $data);
 }
 
-function applyPrices($url, $fuelsortId, $fuelsortProperty) {
+function extractData($url, $fuelsortProperty) {
 	$html = file_get_contents($url);
 	//echo $html;
 	preg_match_all('/var spmResult = (\[[a-zA-Z0-9{}&["_\-:.\/, \\\]+\])/', $html, $result);
 	$decoded_result = json_decode($result[1][0], true);
+
+	$data = array();
+	foreach ($decoded_result as $externalData) {
+		$data[] = array(
+			'mtsk_id' => $externalData['mtsk_id'],
+			'price' => $externalData[$fuelsortProperty],
+			'datetime' => $externalData['datum']
+		);
+	}
 	/*
-		echo '<pre>';
-		var_dump($decoded_result);
-		echo '</pre>';
-		*/
-	$curl = curl_init();
-	$query = http_build_query($decoded_result, 'entry_');
-	$data = array(
-		'data' => $query,
-		'fuelsortId' => $fuelsortId,
-		'fuelsortProperty' => $fuelsortProperty
-	);
+	echo '<pre>';
+	var_dump($data);
+	echo '</pre>';
+	*/
+	return $data;
+}
 
-	curl_setopt($curl, CURLOPT_URL, 'http://klevertanken.pytalhost.de/_rjs/data/php/mts_receiver.php');
-	curl_setopt($curl, CURLOPT_TIMEOUT, 40000);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_HEADER, true);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: multipart/form-data"));
-	curl_setopt($curl, CURLOPT_POST, true);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+function applyData($fuelsortId, $data) {
+	$count = 0;
+	foreach ($data as $entryData) {
+		$locationId = findLocationIdByMtskId($entryData['mtsk_id']);
+		if ($locationId === null) {
+			continue;
+		}
 
-	ob_start();
-	return curl_exec($curl);
-	ob_end_clean();
-	curl_close($curl);
+		$datetime = $entryData['datetime'];
+		$price = parsePrice($entryData['price']);
+
+		if (wasAlreadyAdded($locationId, $fuelsortId, $price, $datetime) === true) {
+			continue;
+		}
+
+		$sql = ""
+			. "INSERT INTO `" . ENTRY_TABLE . "` "
+			. "(`locationId`, `fuelsortId`, `price`, `datetime`, `confirmed`, `deleted`, `mts`) "
+			. "VALUES "
+			. "(" . $locationId . ", " . $fuelsortId . ", " . $price . ", '" . $datetime . "', 0, 0, 1)"
+		;
+		mysql_query($sql);
+
+		$count++;
+	}
+
+	echo $count . ' Eintraege hinzugefuegt.';
+}
+
+function wasAlreadyAdded($locationId, $fuelsortId, $price, $datetime) {
+	$sql = ""
+		. "SELECT `id` "
+		. "FROM `" . ENTRY_TABLE . "` "
+		. "WHERE `locationId` = " . $locationId . " AND `fuelsortId` = " . $fuelsortId . " AND `price` = " . $price . " AND `datetime` = '" . $datetime . "' AND `mts` = 1 "
+	;
+	$result = mysql_query($sql);
+	if ($result !== false) {
+		if (mysql_fetch_assoc($result) !== false) {
+			return true;
+		}
+
+		return false;
+	}
+
+	return false;
+}
+
+function parsePrice($priceString) {
+	// price has the format x.yz9. We don't need the last number.
+	$priceString = substr($priceString, 0, -1);
+	return (float) $priceString;
+}
+
+function findLocationIdByMtskId($mtsk_id) {
+	$sql = ""
+		. "SELECT `id` "
+		. "FROM `" . LOCATION_TABLE . "` "
+		. "WHERE `mtsk_id` = '" . $mtsk_id . "' "
+	;
+
+	$result = mysql_query($sql);
+	if ($result !== false) {
+		$row = mysql_fetch_assoc($result);
+
+		if ($row === false) {
+			return null;
+		}
+
+		return (int) $row['id'];
+	}
+
+	return null;
+}
+
+function sendMail($subject, $mailBody) {
+	$mailHeader = 'MIME-Version: 1.0' . "\r\n";
+	$mailHeader .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$mailHeader .= 'From: Schildi <philippe.schild@googlemail.com>' . "\r\n";
+
+	mail('philippe.schild@googlemail.com', $subject, $mailBody, $mailHeader);
 }
