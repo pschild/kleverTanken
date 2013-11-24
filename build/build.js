b3846927a9b250c0a7b33fde71954ef69db5cf5a
Schildi <philippe.schild@googlemail.com>
2013-11-24 15:10:42 +0100

    Kommentare werden beim Builden in der main.js nun herausgefiltert

--- build/build.js
+++ build/build.js
@@ -1,6 +1,9 @@
 ({
-    baseUrl: '../src/scripts',
-    mainConfigFile: '../src/scripts/main.js',
-    name: 'main',
-    out: '../target/scripts/main.js'
+	baseUrl: '../src/scripts',
+	mainConfigFile: '../src/scripts/main.js',
+	name: 'main',
+	out: '../target/scripts/main.js',
+
+	/* Delete all license comments */
+	preserveLicenseComments: false
 })
